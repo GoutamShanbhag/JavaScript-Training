3d73b70adeb2758aa53bb48607de90b0446232f7
GoutamShanbhag <goutam.s@surya-soft.com>
2022-08-11 17:24:13 +0530

    Adding checkValidPassword function

---
+++ script.js
@@ -0,0 +1,18 @@
+const isValidFunction = (passWord, userName)=>{
+    if(passWord.length<8){
+        return false;
+    }
+
+    if(passWord.includes(" ")){
+        return false;
+    }
+
+    if(passWord.indexOf(userName) !==-1){
+        return false;
+    }
+
+    return true;
+}
+
+
+console.log(isValidFunction("llllPassword","Hey"));
