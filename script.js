7e5701f30d30cb38a9657b7c9d1caf082b749a34
GoutamShanbhag <goutam.s@surya-soft.com>
2022-08-12 09:47:21 +0530

    Adding three functions avg(), pangram() and getCard()

--- script.js
+++ script.js
@@ -16,5 +16,57 @@ const isValidFunction = (passWord, userName)=>{
     return true;
 }
 
+//Function to get the average of numbers
+const avg = (nums)=>{
+    let sum =0;
+    for(let i of nums){
+        sum+=i;
+    }
+    let average = sum / nums.length;
+    return average;
+}
+
+//Function to check pangram
+const pangram = (str)=>{
+    const allLettes = "abcdefghijklmnopqrstuvwxyz";
+    str = str.toLowerCase();
+
+    for(let i of str){
+        if(!allLettes.includes(i)){
+            return false;
+        }
+
+        
+    }
+    return true;
+    
+}
+
+
+const getCard = ()=>{
+    const values = [
+        '2','3','4','5','6','7','8','9','A','J','K','Q'
+    ]
+    const suit = [
+        'Heart','spade','clubs','diamonds',
+    ]
+
+
+    let val = Math.floor(Math.random() * values.length);
+    let suitVal = Math.floor(Math.random()*suit.length);
+
+
+    const result = {
+        value : values[val],
+        suit : suit[suitVal],
+    }
+
+    return result;
+}
+
+
 
 console.log(isValidFunction("llllPassword","Hey"));
+console.log(avg([1,2,3,44,5]));
+console.log(pangram("oxing wizards jump quickly"));
+console.log(getCard())
